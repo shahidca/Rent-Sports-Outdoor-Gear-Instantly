4d5e20392a659531083d8fd66bac4356139d4de8
Md. Shahid Hossain <mdshahidca123@gmail.com>
2026-07-30 22:54:38 +0600

    Folder Structure Created Successfully

---
+++ src/constants/api.ts
@@ -0,0 +1,10 @@
+export const API = {
+  AUTH: "/auth",
+  GEAR: "/gear",
+  RENTALS: "/rentals",
+  PAYMENTS: "/payments",
+  PROVIDER: "/provider",
+  ADMIN: "/admin",
+  REVIEWS: "/reviews",
+  CATEGORIES: "/categories",
+};
---
+++ src/constants/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/auth/login",
+  REGISTER: "/auth/register",
+
+  GEAR: "/gear",
+
+  CUSTOMER_DASHBOARD: "/dashboard/customer",
+  PROVIDER_DASHBOARD: "/dashboard/provider",
+  ADMIN_DASHBOARD: "/dashboard/admin",
+};
